0e0eb41565a85001baab4075ff639d4a79e7f48e
takenokoya9973 <62060755+takenoko9973@users.noreply.github.com>
2025-05-31 06:14:26 +0000

    AutoScaleの継承先変更

--- src/rheed_segmentation/transforms/auto_scale.py
+++ src/rheed_segmentation/transforms/auto_scale.py
@@ -1,33 +1,22 @@
 from typing import Any
 
 import numpy as np
-from albumentations.core.transforms_interface import BasicTransform
+from albumentations.core.transforms_interface import ImageOnlyTransform
 from PIL import Image
 
 
-class AutoScaleTransform(BasicTransform):
-    def __init__(self) -> None:
-        super().__init__(p=1.0)
+class AutoScaleTransform(ImageOnlyTransform):
+    def __init__(self, p: float = 1.0) -> None:
+        super().__init__(p=p)
 
     def apply(self, img: np.ndarray, **_: Any) -> np.ndarray:  # noqa: ANN401
-        # PIL.Image に変換してビット深度を取得
         pil_image = Image.fromarray(img)
 
         bit_depth = self.get_image_bit_depth(pil_image)
         max_pixel_value = 2**bit_depth
 
+        # [0, 1] に正規化
         return img.astype(np.float32) / max_pixel_value
-
-    def apply_to_mask(self, mask: np.ndarray, **_: Any) -> np.ndarray:  # noqa: ANN401
-        # マスクはそのまま
-        return mask
-
-    @property
-    def targets(self) -> dict:
-        return {
-            "image": self.apply,
-            "mask": self.apply_to_mask,
-        }
 
     def get_transform_init_args_names(self) -> tuple[str, ...]:
         return ()
